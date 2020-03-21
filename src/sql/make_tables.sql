5d0cb60c7a63f58f6ada77f6856ffbdd06e8b82b
Hamish Rodda <hamishrodda@icloud.com>
2020-03-21 17:53:35 +1300

    Correct create scripts

--- src/sql/make_tables.sql
+++ src/sql/make_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE symptoms (
   id      TEXT NOT NULL,
   symptom TEXT NOT NULL,
   ts      TIMESTAMP NOT NULL,
-  PRIMARY KEY(symptom_id, symptom)
+  PRIMARY KEY(id, symptom)
 );
 
 CREATE INDEX symptom_by_id ON symptoms USING HASH (id);
@@ -15,5 +15,5 @@ CREATE TABLE interactions (
   ts_uploaded TIMESTAMP NOT NULL 
 );
 
-CREATE INDEX interactions_by_uploaded_geo_occurred_id ON interactions(ts_uploaded, geo, ts_occurred, id);
+CREATE INDEX interactions_by_uploaded_geo_id ON interactions(ts_uploaded, geo, id);
 CREATE INDEX interactions_by_case ON interactions USING HASH(symptom_id);
